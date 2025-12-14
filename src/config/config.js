2f8c4090cff03e0430ab8f0ae5ea437fd6d75f00
Siva Sandeep <sivasandeep.dev@gmail.com>
2025-12-14 13:01:49 +0530

    changed urlp

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 // src/config/config.js
 
-const API_URL = 'http://localhost:5000/api/v1';
-   
+// const API_URL = 'http://localhost:5000/api/v1';
+ const API_URL = 'https://spabillingbackend.up.railway.app/api/v1';                    
+//  https://spabillingbackend.up.railway.app/
+         
 export { API_URL };  
